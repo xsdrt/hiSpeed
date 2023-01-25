8bd6d22c6c314249d4f791ddae281637d9712e49
xsdrt <m_redinger@hotmail.com>
2023-01-24 19:57:20 -0500

    Added some more test to the render_testgo file and also made changes to the render.go file.

--- render/render_test.go
+++ render/render_test.go
@@ -23,11 +23,58 @@ func TestRender_Page(t *testing.T) {
 	}
 	//Above test passed...
 
+	err = testRenderer.Page(w, r, "no-file", nil, nil) //Test method calling for a non-existent file and indicating it should be == nil;
+	if err == nil {                                    // this should fail, which indicates a pass for testing for an err for non existent file to be parsed
+		t.Error("Error rendering non-existent template.", err)
+	}
+
 	testRenderer.Renderer = "jet" //  Tested before adding the page  to testdata/views to make sure it fails; it did....Great...
 	err = testRenderer.Page(w, r, "home", nil, nil)
 	if err != nil {
 		t.Error("Error rendering page.", err)
 	}
 	//Test after add the page passed...OK   //* Just an FYI: use go test -cover . to check how much a folder has been tested;
-	//but this will not tell you what statements themselves.  So create a make file in the root of the project to do this...
+	//but this will not tell you what you have tested in the statements themselves.  So create test coverage Makefile in the root of the project to do this...
+
+	err = testRenderer.Page(w, r, "no-file", nil, nil) //Test method calling for a non-existent jet file and indicating it should be == nil;
+	if err == nil {                                    // this should fail, which indicates a test pass for testing for an err for non-existent jet file to be parsed...
+		t.Error("Error rendering non-existent jet template.", err)
+	}
 }
+
+func TestRender_GoPage(t *testing.T) {
+	w := httptest.NewRecorder()
+	r, err := http.NewRequest("GET", "/url", nil)
+	if err != nil {
+		t.Error(err)
+	}
+
+	testRenderer.Renderer = "go"
+	testRenderer.RootPath = "./testdata"
+
+	err = testRenderer.Page(w, r, "home", nil, nil)
+	if err != nil {
+		t.Error("Error rendering page.", err)
+	}
+
+}
+
+func TestRender_JetPage(t *testing.T) {
+	w := httptest.NewRecorder()
+	r, err := http.NewRequest("GET", "/url", nil)
+	if err != nil {
+		t.Error(err)
+	}
+
+	testRenderer.Renderer = "jet"
+
+	err = testRenderer.Page(w, r, "home", nil, nil)
+	if err != nil {
+		t.Error("Error rendering page.", err)
+	}
+
+}
+
+//Some test cmd for the render file : run tests in render (go test .), run test verbose (go test -v .)
+// Some test cmd if we go up one to hiSpeed : cd .. , then (make cover) opens browser to show what has
+// been covered in the test(green), and what has not (red).
